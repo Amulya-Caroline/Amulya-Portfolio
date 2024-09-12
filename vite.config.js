2c0a19d0aba824862d7c45ca2f88ae75ba9b4f22
Amulya-Caroline <143417255+Amulya-Caroline@users.noreply.github.com>
2024-09-13 03:11:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Explicitly define the output directory
+    rollupOptions: {
+      input: './index.html',  // Make sure Vite knows where the root of the app is
+    }
+  }
 })
