0f7ae17645aa9a6fd24a6b82b00f5d56376cf305
Amulya-Caroline <azarpentagonspace@gmail.com>
2024-09-18 01:38:15 +0530

    Update Jekyll configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   base: './',
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',  // Explicitly define the output directory
+//     rollupOptions: {
+//       input: './index.html',  // Make sure Vite knows where the root of the app is
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Explicitly define the output directory
-    rollupOptions: {
-      input: './index.html',  // Make sure Vite knows where the root of the app is
-    }
-  }
 })
